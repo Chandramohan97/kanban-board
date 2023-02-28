2ef7b5cf6ecd2be6d3047ecc56c1a81f62b3177f
Chandramohan97 <chandramsriv@gmail.com>
2023-02-28 18:37:28 +0530

    style and feat : Board component.

---
+++ src/Components/Board/Board.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './Board.css'
+import { MoreHorizontal } from "react-feather";
+import Card from "../Cards/Card";
+import Editable from "../Editable/Editable";
+
+function Board(){
+    return(
+        <div className="board">
+            <div className="board_top">
+                <p>To Do <span>2</span></p>
+                    <MoreHorizontal/>
+            </div>
+            <div className="board_cards custom-scroll" >
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Editable 
+                  displayClass="boards_cards_add"
+                  text="Add Card"
+                  placeholder="Enter card title"  
+                />
+            </div>
+           
+        </div>
+    )
+}
+
+export default Board;
