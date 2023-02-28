c956e5d59d54d63220e01fcc2a8d1e2a913ba7c0
Chandramohan97 <chandramsriv@gmail.com>
2023-02-28 18:38:03 +0530

    feat : chip style and feat

---
+++ src/Chip/Chip.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Chip.css'
+import { X } from 'react-feather';
+function Chip(props){
+    console.log(props.close)
+    return(
+        <div className='chip' style={{backgroundColor:props.color}}>
+            {props.text}
+            {/* {props.close && <X onClick={() => props.onClose ? props.close() : " "} />} */}
+            {/* {props.text} */}
+        </div>
+    )
+}
+
+export default Chip;
