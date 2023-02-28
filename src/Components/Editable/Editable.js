eee84cf7b7b8edb2df24351ec4a84d8d95a9d7ca
Chandramohan97 <chandramsriv@gmail.com>
2023-02-28 18:57:23 +0530

    Editable button below each board : feat and styling

---
+++ src/Components/Editable/Editable.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {X} from "react-feather"
+// import { Editable. } from '@chakra-ui/react';
+import './Editable.css'
+
+export default function Editable(props){
+    const [showEdit,setShowEdit] = React.useState(false);
+
+    console.log(showEdit)
+    return (
+        <div className="editable">
+            {showEdit ? (            
+            <form className="editable_edit"
+             onSubmit={(event) =>{
+                event.preventDefault();
+                if(props.onSubmit)props.onSubmit();
+             }}
+            >
+                <input
+                 type="text" 
+                 placeholder={props.placeholder || "Enter item"}/>
+                <div className="editable_edit_footer">
+                    <button type="submit">{props.buttonText || "Add"}</button>
+                    <X onClick={()=>setShowEdit(false)}/>
+                </div>   
+            </form>) : 
+            <button className='editable_display `${props.displayClass}`' onClick={()=> setShowEdit(true)}> {props.text || "Add Item"}</button>
+        }          
+        </div>
+    )
+}
