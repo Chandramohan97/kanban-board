c926f3c421e6c3881636b3ae8f3db6c3e6f6e6e8
Chandramohan97 <chandramsriv@gmail.com>
2023-03-06 02:09:33 +0530

    style : designing of Dropdown options on Kanban

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -3,14 +3,34 @@ import './Board.css'
 import { MoreHorizontal } from "react-feather";
 import Card from "../Cards/Card";
 import Editable from "../Editable/Editable";
+import Dropdown from "../Dropdown/Dropdown";
 
 function Board(){
+
+    const [showDropDown,setShowDropDown] = React.useState(false)
+
     return(
-        <div className="board">
-            <div className="board_top">
+        <div className="board" onClick={()=>setShowDropDown(false)}>
+            <div className="board_top" onClick={()=>setShowDropDown(false)}>
                 <p>To Do <span>2</span></p>
-                    <MoreHorizontal/>
+                 <MoreHorizontal onClick={()=>setShowDropDown(true)}/>
+                 {  
+                    showDropDown && (
+                        <Dropdown value="Delete">
+                        </Dropdown> 
+                    )
+                 }
             </div>
+                    {/* <div className="board_top_more">
+                        <Dropdown >
+                            <div className="board_dropdown">
+                                <p>Delete Board</p>
+                            </div>
+
+                        </Dropdown>
+                 
+                    </div>  */}
+            
             <div className="board_cards custom-scroll" >
                 <Card/>
                 <Card/>
